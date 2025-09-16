5a66171225219f56adb36b6ce369a8d77966472c
saurhub-git <saurabh95yadav@gmail.com>
2025-09-16 17:53:33 +0530

    feat: allow users to view todo stats in footer

--- src/App.jsx
+++ src/App.jsx
@@ -28,7 +28,7 @@ function App() {
         onDeleteItem={handleDeleteItem}
         onToggleItem={handleToggleItem}
       />
-      <Stats />
+      <Stats items={items} />
     </div>
   );
 }
@@ -106,10 +106,23 @@ function Item({ item, onDeleteItem, onToggleItem }) {
     </li>
   );
 }
-function Stats() {
+function Stats({ items }) {
+  const completedtasks = items.filter((item) => item.packed).length;
+  const percentage = Math.round((completedtasks / items.length) * 100);
   return (
     <footer className="stats">
-      <em>🎒 You have X items on your list, and you already packed X (X%)</em>
+      <em>
+        {percentage === 100
+          ? "You have done it, You're a GOAT 🐐 Now Enjoy!"
+          : `🎒 You have ${
+              items.length > 0 ? items.length : "no"
+            } tasks on your list,
+        ${
+          items.length > 0
+            ? `and you have completed ${completedtasks} tasks (${percentage}%)`
+            : " please add something in the list."
+        }`}
+      </em>
     </footer>
   );
 }
